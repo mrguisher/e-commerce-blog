8e6f09c43fec2b187b73e024a01789743a1edc83
Mateusz <mrpeciak@gmail.com>
2018-12-08 16:59:49 +0100

    change phone nav link

--- index.js
+++ index.js
@@ -1,62 +1,44 @@
 $(document).ready(nav_menu);
-
 var status = 1;
 
 function nav_menu() {
 
-    $(".nav-bar__navigation").click(function() {
+    $(".nav-bar__toggle").click(function() {
 
         if(status == 1 ){
-        
             $(".nav-bar__nav-phone").animate({
                 opacity: '0'
             });
-
             $(".nav-bar__nav-phone-crossed").animate({
                 opacity: '1'
             });
-
             $(".nav-bar__nav-phone-crossed").css(
                 "background-color", "transparent"
             );
-
             $(".navigation-phone").animate({
                 left: '0'    
             });
 
-
-            
             $('body').addClass('lock-scroll');
-            
-
 
             status = 0;
         }
         else {
-            
             $(".nav-bar__nav-phone").animate({
                 opacity: '1'
             });
-
             $(".nav-bar__nav-phone-crossed").animate({
-                opacity: '0'
-                
+                opacity: '0' 
             });
-
             $(".nav-bar__nav-phone-crossed").css(
                 "background-color", "transparent"
             );
-
             $(".navigation-phone").animate({
                 left: '-100%'    
             });
-
-
             $('body').removeClass('lock-scroll');
 
             status = 1;
         }
-    
     });
-
 };
